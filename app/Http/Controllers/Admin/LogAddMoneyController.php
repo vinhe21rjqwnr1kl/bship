caff827b29d5723386c228f0b6758cb7d673ba19
thanhvotrung <thanhvotrung68@gmail.com>
2024-05-27 19:25:35 +0700

    version 1.1.11

--- app/Http/Controllers/Admin/LogAddMoneyController.php
+++ app/Http/Controllers/Admin/LogAddMoneyController.php
@@ -3,25 +3,50 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Driver;
+use App\Models\LogAddMoneyCashin;
 use App\Models\LogAddMoneyCashout;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class LogAddMoneyController extends Controller
 {
-    public function index_cashout(Request $request) {
-        $page_title = __('Danh sách yêu cầu nạp tiền');
+    public function cashoutIndex(Request $request)
+    {
+        $page_title = __('Danh sách yêu cầu rút tiền');
 
-        $resultQuery = LogAddMoneyCashout::query()->with(['user_data']);
+        $resultQuery = LogAddMoneyCashout::query();
 
-        $resultQuery->whereHas('user_data', function ($query) use ($request) {
-            if ($request->filled('phone')) {
-                $query->where('phone', 'like', "%{$request->input('phone')}%");
-            }
-            if ($request->filled('name')) {
-                $query->where('name', 'like', "%{$request->input('name')}%")
-                    ->orWhere('email', 'like', "%{$request->input('name')}%");
-            }
-        });
+        $resultQuery->with(['user_data', 'user_driver_data'])
+            ->where(function ($query) use ($request) {
+                $query->where(function ($subQuery) use ($request) {
+                    $subQuery->whereHas('user_data', function ($userDataQuery) use ($request) {
+                        $userDataQuery->where('user_type', 1); // Check if user_type is 1 (customer)
+                        if ($request->filled('phone')) {
+                            $userDataQuery->where('phone', 'like', "%{$request->input('phone')}%");
+                        }
+                        if ($request->filled('name')) {
+                            $userDataQuery->where(function ($nameQuery) use ($request) {
+                                $nameQuery->where('name', 'like', "%{$request->input('name')}%")
+                                    ->orWhere('email', 'like', "%{$request->input('name')}%");
+                            });
+                        }
+                    });
+                })->orWhere(function ($subQuery) use ($request) {
+                    $subQuery->whereHas('user_driver_data', function ($userDriverDataQuery) use ($request) {
+                        $userDriverDataQuery->where('user_type', 2); // Check if user_type is 2 (driver)
+                        if ($request->filled('phone')) {
+                            $userDriverDataQuery->where('phone', 'like', "%{$request->input('phone')}%");
+                        }
+                        if ($request->filled('name')) {
+                            $userDriverDataQuery->where(function ($nameQuery) use ($request) {
+                                $nameQuery->where('name', 'like', "%{$request->input('name')}%")
+                                    ->orWhere('email', 'like', "%{$request->input('name')}%");
+                            });
+                        }
+                    });
+                });
+            });
 
         //check tai xe thuoc dai ly
         $current_user = auth()->user();
@@ -34,11 +59,151 @@ class LogAddMoneyController extends Controller
         $resultQuery->orderBy($sortBy, $direction);
         $LogAddMoneyRequest = $resultQuery->paginate(config('Reading.nodes_per_page'));
 
-//        return response()->json($LogAddMoneyRequest);
         return view('admin.log_add_money.cashout', compact('LogAddMoneyRequest', 'page_title'));
     }
 
-    public function index_cashin(Request $request) {
+    public function cashinIndex(Request $request)
+    {
+        $page_title = __('Danh sách yêu cầu nạp tiền');
 
+        $resultQuery = LogAddMoneyCashin::query();
+
+        $resultQuery->with(['user_data', 'user_driver_data'])
+            ->where(function ($query) use ($request) {
+                $query->where(function ($subQuery) use ($request) {
+                    $subQuery->whereHas('user_data', function ($userDataQuery) use ($request) {
+                        $userDataQuery->where('user_type', 1); // Check if user_type is 1 (customer)
+                        if ($request->filled('phone')) {
+                            $userDataQuery->where('phone', 'like', "%{$request->input('phone')}%");
+                        }
+                        if ($request->filled('name')) {
+                            $userDataQuery->where(function ($nameQuery) use ($request) {
+                                $nameQuery->where('name', 'like', "%{$request->input('name')}%")
+                                    ->orWhere('email', 'like', "%{$request->input('name')}%");
+                            });
+                        }
+                    });
+                })->orWhere(function ($subQuery) use ($request) {
+                    $subQuery->whereHas('user_driver_data', function ($userDriverDataQuery) use ($request) {
+                        $userDriverDataQuery->where('user_type', 2); // Check if user_type is 2 (driver)
+                        if ($request->filled('phone')) {
+                            $userDriverDataQuery->where('phone', 'like', "%{$request->input('phone')}%");
+                        }
+                        if ($request->filled('name')) {
+                            $userDriverDataQuery->where(function ($nameQuery) use ($request) {
+                                $nameQuery->where('name', 'like', "%{$request->input('name')}%")
+                                    ->orWhere('email', 'like', "%{$request->input('name')}%");
+                            });
+                        }
+                    });
+                });
+            });
+
+        //check tai xe thuoc dai ly
+        $current_user = auth()->user();
+        $driveData["agency_id"] = $current_user->agency_id;
+        if ($driveData["agency_id"] > 0) {
+            $resultQuery->where('agency_id', '=', $driveData["agency_id"]);
+        }
+        $sortBy = $request->get('sort') ? $request->get('sort') : 'id';
+        $direction = $request->get('direction') ? $request->get('direction') : 'desc';
+        $resultQuery->orderBy($sortBy, $direction);
+        $LogAddMoneyRequest = $resultQuery->paginate(config('Reading.nodes_per_page'));
+
+        return view('admin.log_add_money.cashin', compact('LogAddMoneyRequest', 'page_title'));
     }
+
+    public function cashoutAccept(Request $request, $id)
+    {
+        try {
+            DB::beginTransaction();
+
+            $log = LogAddMoneyCashout::query()->where('id', '=', $id)->first();
+            if (!$log) {
+                return redirect()->back()->with('error', __('Yêu cầu không tồn tại'));
+            }
+
+            $driver = Driver::query()->where('id', '=', $log->user_id)->first();
+            if (!$driver) {
+                return redirect()->back()->with('error', __('Người dùng không tồn tại'));
+            }
+
+            $log->status = 2;
+            $driver->money -= $log->amount;
+
+            $log->save();
+            $driver->save();
+
+            DB::commit();
+            return redirect()->back()->with('success', 'Thành công');
+
+        } catch (\Exception $ex) {
+            DB::rollBack();
+            return redirect()->back()->with('error', __('Quá trình xảy ra lỗi'));
+        }
+    }
+
+    public function cashinAccept(Request $request, $id)
+    {
+        try {
+            DB::beginTransaction();
+
+            $log = LogAddMoneyCashin::query()->where('id', '=', $id)->first();
+            if (!$log) {
+                return redirect()->back()->with('error', __('Yêu cầu không tồn tại'));
+            }
+
+            $driver = Driver::query()->where('id', '=', $log->user_id)->first();
+            if (!$driver) {
+                return redirect()->back()->with('error', __('Người dùng không tồn tại'));
+            }
+
+            $log->status = 2;
+            $driver->money += $log->amount;
+
+            $log->save();
+            $driver->save();
+
+            DB::commit();
+            return redirect()->back()->with('success', 'Thành công');
+
+        } catch (\Exception $ex) {
+            DB::rollBack();
+            return redirect()->back()->with('error', __('Quá trình xảy ra lỗi'));
+        }
+    }
+
+    public function cashoutReject(Request $request, $id)
+    {
+        try {
+            $log = LogAddMoneyCashout::query()->where('id', '=', $id)->first();
+            if (!$log) {
+                return redirect()->back()->with('error', __('Yêu cầu không tồn tại'));
+            }
+            $log->status = 0;
+            $log->save();
+            return redirect()->back()->with('success', 'Từ chối yêu cầu thành công');
+
+        } catch (\Exception $ex) {
+            return redirect()->back()->with('error', __('Quá trình xảy ra lỗi'));
+        }
+    }
+
+    public function cashinReject(Request $request, $id)
+    {
+        try {
+            $log = LogAddMoneyCashin::query()->where('id', '=', $id)->first();
+            if (!$log) {
+                return redirect()->back()->with('error', __('Yêu cầu không tồn tại'));
+            }
+            $log->status = 0;
+            $log->save();
+            return redirect()->back()->with('success', 'Từ chối yêu cầu thành công');
+
+        } catch (\Exception $ex) {
+            return redirect()->back()->with('error', __('Quá trình xảy ra lỗi'));
+        }
+    }
+
+
 }
--- app/Models/LogAddMoneyCashin.php
+++ app/Models/LogAddMoneyCashin.php
@@ -4,13 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class LogAddMoneyCashin extends Model
 {
     use HasFactory;
-    protected $table = 'log_add_money_cashout';
+    protected $table = 'log_add_money_cashin';
     protected $fillable = [];
 
+    public function user_data() : BelongsTo {
+        return $this->belongsTo(UserB::class, 'user_id', 'id');
+    }
 
+    public function user_driver_data() : BelongsTo {
+        return $this->belongsTo(Driver::class, 'user_id', 'id');
+    }
 }
 
